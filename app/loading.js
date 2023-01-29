980ca341f06c57663f02b47d0a8a24abe1e9909a
Ali Jendoubi <alijandhd@gmail.com>
2023-01-29 14:18:28 +0100

    Update loading

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div>loading</div>
+    <div>
+    <div className="flex justify-center bg-base-200 items-center h-screen">
+      <div className="flex flex-col items-center gap-2">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-4 border-primary/60" />
+      </div>
+    </div>
+  </div>
   )
 }
 
