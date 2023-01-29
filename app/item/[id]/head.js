080485636f9ec942d6364efa7175ea3bf931ab79
Ali Jendoubi <alijandhd@gmail.com>
2023-01-29 14:01:09 +0100

    Update head.js

--- app/item/[id]/head.js
+++ app/item/[id]/head.js
@@ -1,26 +1,29 @@
-async function getCategory() {
+async function getCategory(id) {
   const res = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php',  { cache: 'no-store' });
-   return res.json()
+  let result = await res.json();
+  let data = result.categories.find((item) => item.strCategory == id);
+  console.log(data)
+  return data
 }
 
 export default async function Head({params: {id}}) {
-  const Category = await getCategory()
+  const Category = await getCategory(id)
   return (
     <>
-      <title>{Category.categories.find((item) => item.strCategory == id).strCategory}</title>
-<meta name="title" content={Category.categories.find((item) => item.strCategory == id).strCategory} />
-<meta name="description" content={Category.categories.find((item) => item.strCategory == id).strCategoryDescription.slice(0,50)+'...'} />
+      <title>{Category.strCategory}</title>
+<meta name="title" content={Category.strCategory} />
+<meta name="description" content={Category.strCategoryDescription.slice(0,50)+'...'} />
 <meta property="og:type" content="website" />
-<meta property="og:url" content={`https://next-js-13-seo.vercel.app/item/`+Category.categories.find((item) => item.strCategory == id).strCategory} />
-<meta property="og:title" content={Category.categories.find((item) => item.strCategory == id).strCategory} />
-<meta property="og:description" content={Category.categories.find((item) => item.strCategory == id).strCategoryDescription.slice(0,50)+'...'}  />
-<meta property="og:image" content={Category.categories.find((item) => item.strCategory == id).strCategoryThumb}  />
+<meta property="og:url" content={`https://next-js-13-seo.vercel.app/item/`+Category.strCategory} />
+<meta property="og:title" content={Category.strCategory} />
+<meta property="og:description" content={Category.strCategoryDescription.slice(0,50)+'...'}  />
+<meta property="og:image" content={Category.strCategoryThumb}  />
 
 <meta property="twitter:card" content="summary_large_image" />
-<meta property="twitter:url" content={`https://next-js-13-seo.vercel.app/item/`+Category.categories.find((item) => item.strCategory == id).strCategory}  />
-<meta property="twitter:title" content={Category.categories.find((item) => item.strCategory == id).strCategory} />
-<meta property="twitter:description" content={Category.categories.find((item) => item.strCategory == id).strCategoryDescription.slice(0,50)+'...'} />
-<meta property="twitter:image" content={Category.categories.find((item) => item.strCategory == id).strCategoryThumb} />
+<meta property="twitter:url" content={`https://next-js-13-seo.vercel.app/item/`+Category.strCategory}  />
+<meta property="twitter:title" content={Category.strCategory} />
+<meta property="twitter:description" content={Category.strCategoryDescription.slice(0,50)+'...'} />
+<meta property="twitter:image" content={Category.strCategoryThumb} />
     </>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body data-theme="night">{children}</body>
     </html>
   )
 }
